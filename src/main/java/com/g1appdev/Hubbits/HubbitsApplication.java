9b2c5e89d29141be8cf750903f57164d966ebc01
stellaurr <stelamarizlatras@gmail.com>
2024-11-07 15:34:55 +0800

    Adopt A Pet (Initial User & Admin)

--- src/main/java/com/g1appdev/Hubbits/HubbitsApplication.java
+++ src/main/java/com/g1appdev/Hubbits/HubbitsApplication.java
@@ -1,4 +1,4 @@
-package com.g1appdev.Hubbits;
+	package com.g1appdev.Hubbits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
