b6f1ce60970000bb6d0de1cb987d89adc018eb62
User <alastorrlc@gmail.com>
2024-11-07 21:51:14 +0800

    Donation + NewsFeed (front and back)

--- src/main/java/com/g1appdev/Hubbits/repository/NewsFeedRepository.java
+++ src/main/java/com/g1appdev/Hubbits/repository/NewsFeedRepository.java
@@ -1,8 +1,9 @@
 package com.g1appdev.Hubbits.repository;
-import com.g1appdev.Hubbits.entity.User;
+
+import com.g1appdev.Hubbits.entity.NewsFeedEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface NewsFeedRepository extends JpaRepository<NewsFeedEntity, Long> {
 }
