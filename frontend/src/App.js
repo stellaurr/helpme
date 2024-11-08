af2ec723e448e0c4fc884c73c8cbd9600f6b647b
Kato-Neko <ng112403@gmail.com>
2024-11-09 02:19:03 +0800

    working navbar, called navbar in the app.js itself and removed it from the other js (login, signup). also modified newsfeed to display the search above navbar so that it'd appear only on that page. sorry, i talked too much

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,74 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import UserDashboard from './components/UserDashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import UserDashboard from "./components/UserDashboard";
 import Home from "./components/Home";
 import LostAndFound from "./components/LostAndFound";
 import Sponsor from "./components/Sponsor";
-import AdoptionForm from './components/AdoptionForm';
-import AdoptionList from './components/AdoptionList';
-import DonationForm from './components/DonationForm';
-import DonationTable from './components/DonationTable';
+import AdoptionForm from "./components/AdoptionForm";
+import AdoptionList from "./components/AdoptionList";
+import DonationForm from "./components/DonationForm";
+import DonationTable from "./components/DonationTable";
 import VolunteerSignUp from "./components/VolunteerSignUpList";
 import CreateOpportunity from "./components/CreateOpportunity";
 import Volunteer from "./components/Volunteer";
 import VolunteerOpportunities from "./components/VolunteerOpportunities";
 import OpportunityDetail from "./components/OpportunityDetail"; // Ensure this import is present
 import NoMatch from "./components/NoMatch";
-
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Navbar from "./components/Navbar";
 
 import Profile from "./components/Profile";
 
 // import ReactDOM from "react-dom";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#675BC8",
+    },
+  },
+});
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
       <Router>
         <div>
+          <Navbar />
           <Routes>
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-              <Route path="/users" element={<UserDashboard/>} />
-              <Route path="/profile" element={<Profile/>} />
-            <Route path="/" element={<Login />} />  {/* Default to login */}
+            <Route path="/users" element={<UserDashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/" element={<Login />} /> {/* Default to login */}
             <Route path="/home" element={<Home />} />
             <Route path="/lost-and-found" element={<LostAndFound />} />
             <Route path="/sponsor" element={<Sponsor />} />
             <Route path="/adoption" element={<AdoptionForm />} />
-            <Route path="/adopt_dash" element={<AdoptionList/>} />
+            <Route path="/adopt_dash" element={<AdoptionList />} />
             <Route path="/donation" element={<DonationForm />} />
-            <Route path="/donation_dash" element={<DonationTable/>} />
+            <Route path="/donation_dash" element={<DonationTable />} />
             <Route path="/volunteer" element={<Volunteer />} />
-          <Route path="/admin/manage-opportunities" element={<VolunteerOpportunities />} />
-          <Route path="/admin/manage-volunteers" element={<VolunteerSignUp />} />
-          <Route path="/opportunity/:id" element={<OpportunityDetail />} /> 
-          <Route path="/book" element={<CreateOpportunity />} />
-          <Route path="" element={<NoMatch />} />
-
-
+            <Route
+              path="/admin/manage-opportunities"
+              element={<VolunteerOpportunities />}
+            />
+            <Route
+              path="/admin/manage-volunteers"
+              element={<VolunteerSignUp />}
+            />
+            <Route path="/opportunity/:id" element={<OpportunityDetail />} />
+            <Route path="/book" element={<CreateOpportunity />} />
+            <Route path="" element={<NoMatch />} />
           </Routes>
         </div>
       </Router>
+    </ThemeProvider>
   );
 }
 
 //ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
-
-
--- frontend/src/components/NewsFeed.js
+++ frontend/src/components/NewsFeed.js
@@ -1,6 +1,12 @@
-// src/components/NewsFeed.js
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Typography, TextField, Button } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  Box,
+} from "@mui/material";
 import axios from "axios";
 import CreatePostDialog from "./CreatePostDialog";
 import PostCard from "./PostCard";
@@ -15,7 +21,9 @@ const NewsFeed = () => {
 
   const fetchLostItems = async () => {
     try {
-      const response = await axios.get("http://localhost:8080/api/lostandfound");
+      const response = await axios.get(
+        "http://localhost:8080/api/lostandfound"
+      );
       setLostItems(response.data);
     } catch (error) {
       console.error("Error fetching lost items:", error);
@@ -51,88 +59,126 @@ const NewsFeed = () => {
   );
 
   return (
-    <Container sx={{ mt: 20 }}>
-      <Grid
-        container
-        justifyContent="space-between"
-        alignItems="center"
-        sx={{ mb: 2 }}
+    <>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "90px",
+          left: "70%", // Center the Box horizontally
+          transform: "translateX(-50%)", // Correctly center the box
+          zIndex: 10,
+          width: "80%",
+          maxWidth: "400px",
+          padding: "5px",
+        }}
       >
-        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-          LOST AND FOUND
-        </Typography>
-
         <TextField
           variant="outlined"
           placeholder="Search"
-          value={searchQuery} // Bind the value to the search state
-          onChange={handleSearchChange} // Update state on change
-          sx={{ "& .MuiOutlinedInput-root": { height: "40px" } }}
+          value={searchQuery}
+          onChange={handleSearchChange}
+          sx={{
+            width: "100%",
+            backgroundColor: "transparent",
+            "& .MuiOutlinedInput-root": {
+              height: "35px",
+              border: "1px solid white",
+              "&.Mui-focused": {
+                borderColor: "white",
+              },
+            },
+            "& .MuiOutlinedInput-input": {
+              color: "white",
+            },
+            "& .MuiSvgIcon-root": {
+              color: "white",
+            },
+            "& fieldset": {
+              border: "none",
+            },
+          }}
           InputProps={{
             endAdornment: (
-              <Button>
+              <Button
+                sx={{ padding: 0, backgroundColor: "transparent !important" }}
+              >
+                {" "}
                 <SearchIcon />
               </Button>
             ),
           }}
         />
-      </Grid>
+      </Box>
 
-      <Grid
-        container
-        justifyContent="center"
-        sx={{
-          mt: 6,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          textAlign: "center",
-        }}
-      >
-        <TextField
-          variant="outlined"
-          fullWidth
-          multiline
-          rows={2}
-          placeholder="Have you found someone's pet? Or did you lose a pet?"
-          onClick={handleTextFieldClick}
+      {/* Main Content */}
+      <Container sx={{ mt: 5 }}>
+        <Grid
+          container
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ mb: 2 }}
+        >
+          <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+            LOST AND FOUND
+          </Typography>
+        </Grid>
+
+        <Grid
+          container
+          justifyContent="center"
           sx={{
-            maxWidth: 600,
-            mb: 5,
-            backgroundColor: "white",
-            borderRadius: "8px",
+            mt: 6,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
           }}
+        >
+          <TextField
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={2}
+            placeholder="Have you found someone's pet? Or did you lose a pet?"
+            onClick={handleTextFieldClick}
+            sx={{
+              maxWidth: 600,
+              mb: 5,
+              backgroundColor: "white",
+              borderRadius: "8px",
+            }}
+          />
+        </Grid>
+
+        <CreatePostDialog
+          open={openDialog}
+          setOpen={setOpenDialog}
+          fetchLostItems={fetchLostItems}
+          postToEdit={postToEdit}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          setPostToEdit={setPostToEdit}
         />
-      </Grid>
 
-      <CreatePostDialog
-        open={openDialog}
-        setOpen={setOpenDialog}
-        fetchLostItems={fetchLostItems}
-        postToEdit={postToEdit}
-        isEditing={isEditing}
-        setIsEditing={setIsEditing}
-        setPostToEdit={setPostToEdit}
-      />
-
-      <Grid
-        container
-        spacing={2}
-        direction="column"
-        alignItems="center"
-        sx={{ mt: 4 }}
-      >
-        {filteredItems.map((item) => (
-          <Grid item key={item.id} sx={{ width: "100%", maxWidth: 600 }}>
-            <PostCard
-              item={item}
-              fetchLostItems={fetchLostItems}
-              onEdit={handleEditPost}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+        <Grid
+          container
+          spacing={2}
+          direction="column"
+          alignItems="center"
+          sx={{ mt: 4 }}
+        >
+          {filteredItems.map((item) => (
+            <Grid item key={item.id} sx={{ width: "100%", maxWidth: 600 }}>
+              <PostCard
+                item={item}
+                fetchLostItems={fetchLostItems}
+                onEdit={handleEditPost}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
   );
 };
 
